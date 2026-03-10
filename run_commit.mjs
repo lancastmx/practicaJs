b37c8a0aa1ae80063138eef6fc346c87222395e6
lancast <angelprep92@gmail.com>
2026-03-09 20:01:40 -0600

    fix dela run_commit

--- run_commit.mjs
+++
@@ -1,7 +0,0 @@
-import { run } from 'file:///d:/aprenderProgramacion/praticaJs/src/agents/AutoCommitSkill.js';
-
-run().then(result => {
-    console.log("Run result:", result);
-}).catch(err => {
-    console.error("Run error:", err);
-});
