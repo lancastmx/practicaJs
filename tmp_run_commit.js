7f93257579719e6b5edea63d4d66c313497a8110
lancast <angelprep92@gmail.com>
2026-03-17 09:42:38 -0600

    fallo el auto commit

--- tmp_run_commit.js
+++
@@ -1,8 +0,0 @@
-import { run } from './agents/AutoCommitSkill.js';
-
-run().then(result => {
-    console.log(JSON.stringify(result));
-}).catch(err => {
-    console.error(err);
-    process.exit(1);
-});
